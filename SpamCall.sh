2b607cbd9a96ea8eba7cf5849fcc6e0802b6e210
Jeeck007 <88170751+Jeeck007@users.noreply.github.com>
2021-07-30 07:50:25 +0700

    Add files via upload

---
+++ SpamCall.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# install projects
+
+figlet loading | lolcat
+sleep1
+
+git clone https://github.com/MrVirusSpm-07/projects-spam-call
+
+figlet selesai | lolcat
+sleep1
+
+cd projects-spam-call
+sleep1
+
+# Tampilan
+clear
+toilet -f big -F gay SpamCall
+echo "==================================" | lolcat
+echo "[+] Author : Mr.Jeeck	     [+]"
+echo "[+] Wa     : 081392505882    [+]" 
+echo "[+] Fb     : Jecko X Ganz    [+]"
+echo "==================================" | lolcat
+echo
+echo "Gunakan Degan Bijak" | lolcat
+echo "==================================" | lolcat
+echo
+
+python projects.py
+
